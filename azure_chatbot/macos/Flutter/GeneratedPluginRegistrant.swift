9788da4aad482a1a0ae9d52e5705e2caca83f61c
Sami3160 <samibhadgaonkar@gmail.com>
2026-02-10 23:44:22 +0530

    permissions part and bug fixes

--- azure_chatbot/macos/Flutter/GeneratedPluginRegistrant.swift
+++ azure_chatbot/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import audio_session
+import just_audio
+import speech_to_text
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
+  SpeechToTextPlugin.register(with: registry.registrar(forPlugin: "SpeechToTextPlugin"))
 }
